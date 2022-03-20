080a91f4c537a62282e4253e681faa1c7abadbb3
FindHao <find@findhao.net>
2022-03-19 20:54:31 -0400

    update install script

--- bin/install.sh
+++ bin/install.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-
 if [ $# -lt 1 ];then
     echo "usage: bin/build.sh target_install_path"
     exit -1
 fi
 
 source_path=$(pwd)
-
 # CUDA_PATH=/usr/local/cuda
 install_path=$1 
-
 
 if [ ! -d $install_path ]
 then
@@ -18,9 +15,22 @@ then
     mkdir -p $install_path
 fi
 
+echo "Your source code path is " $source_path
+echo "Your install path is " $install_path
 
-echo $source_path
-echo $install_path
+echo "GPUPUNK-> install dependencies"
+cd $install_path
+git clone --depth 1 https://github.com/spack/spack.git
+export SPACK_ROOT=$(pwd)/spack
+source ${SPACK_ROOT}/share/spack/setup-env.sh
+spack install boost@1.76.0 mbedtls@3.1.0
+spack load boost@1.76.0 mbedtls@3.1.0
+
+# Find spack and boost dir
+B=$(spack find --path boost | tail -n 1 | cut -d ' ' -f 3)
+S=${B%/*}
+echo "GPUPUNK-> boost install path is " $B
+
 
 cd ${source_path}/gpu-patch
 make clean
@@ -28,7 +38,7 @@ make PREFIX=${install_path}/gpu-patch CUDA_PATH=$CUDA_PATH install -j 4
 
 cd  ${source_path}/redshow
 make clean
-make PREFIX=${install_path}/redshow BOOST_DIR=/scratch/spack/opt/spack/linux-ubuntu20.04-cascadelake/gcc-9.3.0/boost-1.76.0-bx6o75jbt5g5ngshnrpzlvodqpjnvjxh GPU_PATCH_DIR=${install_path}/gpu-patch DEBUG=1  STATIC_CPP=1 install -j 12 -f Makefile.static
+make PREFIX=${install_path}/redshow BOOST_DIR=$B GPU_PATCH_DIR=${install_path}/gpu-patch DEBUG=1  STATIC_CPP=1 install -j 12 -f Makefile.static
 
 cd ${source_path}/libmonitor
 make clean
@@ -48,8 +58,8 @@ export ENABLE_GPUTRIGGER=1
 export REDSHOW_PATH=${install_path}/redshow
 export GPUPATCH_PATH=${install_path}/gpu-patch
 cd ${source_path}/drcctprof_clients
-./build_clean.sh ; ./build_debug.sh
-cp -r ./DrCCTProf/build_debug ${install_path}/drcctprof-debug
+./build_clean.sh ; ./build.sh
+cp -r ./DrCCTProf/build ${install_path}/drcctprof
 
 cd ${source_path}/cubin_filter
 rm -rf ${source_path}/cubin_filter/build
@@ -61,5 +71,6 @@ make install -j 4
 cd ${source_path}
 # cp -rf ./bin ${install_path}/
 mkdir ${install_path}/bin
+# @FindHao TODO: change to copy before final release
 ln -s ${source_path}/bin/gpupunk ${install_path}/bin/gpupunk
 
